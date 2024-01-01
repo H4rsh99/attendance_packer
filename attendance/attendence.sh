33c96b64d32537ef5541024c652758fdd8e30232
H4rsh99 <91564266+H4rsh99@users.noreply.github.com>
2024-01-01 19:16:05 +0530

    Update attendence.sh

--- attendance/attendence.sh
+++ attendance/attendence.sh
@@ -4,11 +4,12 @@
 repo_url="https://github.com/H4rsh99/attendance_packer.git"
 
 # Clone the repository
+git clone https://github.com/OT-MICROSERVICES/attendance-api.git
 git clone "$repo_url"
 
 # Install apt packages
 ls
-sudo cp ~/attendance/attendance.service /etc/systemd/system/attendance.service
+sudo cp ~/attendance_packer/attendance/attendance.service /etc/systemd/system/attendance.service
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y update && sudo apt -y install  python3
 sudo apt -y update && sudo apt -y install python3-pip
